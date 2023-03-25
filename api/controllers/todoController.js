8a50783a4268f03ac015038194b855a2ee1ce081
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-25 17:11:29 +0200

    Using the abstracted services for fetch data

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -1,24 +1,44 @@
-const initialTodos = require('../data/data');
-const createTimestamp = require('../utils/createTimestamp');
+const uniqid = require('uniqid');
+const todoServices = require('../apiServices/todoServices');
 
+const createTimestamp = require('../utils/createTimestamp');
 const router = require('express').Router();
 
 const getAllTodos = (req, res) => {
-    res.status(200).json(initialTodos);
+    res.status(200).json(todoServices.getAllTodos());
 };
 
 const createTodo = (req, res) => {
     const newTodo = {
+        id: uniqid(),
         title: 'Take a shower',
         description: 'Take a shower or you will repulse all the girls',
         timestamp: createTimestamp(),
         isDone: false,
     };
-    initialTodos.push(newTodo);
-    res.json(initialTodos);
+
+    const todos = todoServices.getAllTodos();
+    todos.push(newTodo);
+    res.json(newTodo);
+};
+
+const updateTodo = (req, res) => {
+    const todo = todoServices.getTodo(req.params.id);
+    todo.isDone = true;
+
+    res.json(todo);
+};
+
+const getOneTodo = (req, res) => {
+    const todo = todoServices.getTodo(req.params.id);
+    res.json(todo);
 };
 
 router.get('/todos', getAllTodos);
+router.get('/todos/:id', getOneTodo);
+
 router.post('/todos', createTodo);
+
+router.put('/todos/:id', updateTodo);
 
 module.exports = router;
