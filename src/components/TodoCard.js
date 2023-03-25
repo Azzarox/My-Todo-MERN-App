cef2e696bded4015c7840b5726a972b3e9f1ad4c
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-25 14:43:37 +0200

    Refactored component

--- src/components/TodoCard.js
+++ src/components/TodoCard.js
@@ -2,13 +2,26 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function TodoCard({ todo }) {
+
+    let buttonComponent = null;
+
+    if (todo.isDone) {
+        buttonComponent = (
+            <Button disabled variant="success">
+                Completed
+            </Button>
+        );
+    } else {
+        buttonComponent = <Button variant="primary">Complete</Button>;
+    }
+
     return (
         <Card>
             <Card.Header>{todo.timestamp}</Card.Header>
             <Card.Body>
                 <Card.Title>{todo.title}</Card.Title>
                 <Card.Text>{todo.description}</Card.Text>
-                <Button variant="primary">Complete</Button>
+                {buttonComponent}
             </Card.Body>
         </Card>
     );
