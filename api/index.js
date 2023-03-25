e8ff2890ac397b917385dc92908bf96ea7b36c2b
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-25 16:08:05 +0200

    Added the router abstraction

--- api/index.js
+++ api/index.js
@@ -2,36 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const cors = require('cors');
-const initialTodos = require('./data/data');
-const createTimestamp = require('./utils/createTimestamp');
+const router = require('./routes');
 
 app.use(express.json());
 app.use(cors()); // Needed for api requests
 
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'MERN Todo App',
-    });
-});
-
-const getAllTodos = (req, res) => {
-    res.status(200).json(initialTodos);
-};
-
-const createTodo = (req, res) => {
-    const newTodo = {
-        title: 'Take a shower',
-        description: "Take a shower or you will repulse all the girls",
-        timestamp: createTimestamp(),
-        isDone: false,
-    };
-    initialTodos.push(newTodo);
-    res.json(initialTodos);
-};
-
-app.get('/api/todos', getAllTodos);
-
-app.post('/api/todos', createTodo);
+app.use('/api', router);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
