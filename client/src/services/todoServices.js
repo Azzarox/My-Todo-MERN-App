326d4a5e712e72ed54d13cb9010112e243165c33
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 21:38:21 +0300

    Added service for call to search by title

--- client/src/services/todoServices.js
+++ client/src/services/todoServices.js
@@ -4,13 +4,20 @@ const routes = {
     allTodos: (filter) => {
         return `/api/todos?filter=${filter}`;
     },
+    allTodosByTitle: (title) => {
+        return `/api/todos/search?title=${title}`;
+    },
 };
 
 export const getAllTodos = (filter) => {
     // This works even if filter is empty string because of the api default /api/todos
     return fetch(apiUrl + routes.allTodos(filter)).then((res) => res.json());
 };
-
+export const getAllTodosByTitle = (title) => {
+    return fetch(apiUrl + routes.allTodosByTitle(title)).then((res) =>
+        res.json()
+    );
+};
 export const completeTodo = (todo) => {
     return fetch(`http://localhost:3001/api/todos/${todo.id}`, {
         method: 'PUT',
