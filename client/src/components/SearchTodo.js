3926c9e0637e12b8d20f72cbf0d748f16130b753
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 22:35:06 +0300

    Added title change on each keystroke

--- client/src/components/SearchTodo.js
+++ client/src/components/SearchTodo.js
@@ -13,13 +13,18 @@ function SearchTodo({ titleHandler }) {
                     <Form
                         onSubmit={(e) => {
                             e.preventDefault();
-                            titleHandler(value);
+                            titleHandler(value); // After pressing enter it will do the on submit call
                         }}
                     >
                         <Form.Control
                             type="text"
                             placeholder="Search for Todo by title"
-                            onChange={(e) => setValue(e.target.value)}
+                            onChange={(e) => {
+                                setValue(e.target.value);
+                                titleHandler(value);
+                                // If I add titleHandler(value) here
+                                // It will call the api on each keystroke
+                            }}
                         />
                     </Form>
                 </Accordion.Body>
