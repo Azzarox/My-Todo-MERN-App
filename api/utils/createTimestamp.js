3e1443d67f154e2eb23020c7981cc5e2fefc781a
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 11:02:56 +0300

    Function creates parsable timestamps (to compare)

--- api/utils/createTimestamp.js
+++ api/utils/createTimestamp.js
@@ -1,9 +1,24 @@
+// function createTimestamp() {
+//     const currentDate = new Date();
+//     const timestamp = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()} GMT${
+//         -currentDate.getTimezoneOffset() / 60
+//     }`;
+
+//     return timestamp;
+// }
+
 function createTimestamp() {
     const currentDate = new Date();
-    const timestamp = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()} GMT${
-        -currentDate.getTimezoneOffset() / 60
-    }`;
-
+    const options = {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'UTC',
+    };
+    const timestamp = currentDate.toLocaleDateString('en-GB', options);
     return timestamp;
 }
 
