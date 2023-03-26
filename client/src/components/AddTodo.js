f412b250bf02d116c92e75e41141fe978be60690
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 14:55:28 +0300

    Added AddTodo component and css

---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,10 @@
+import styles from './AddTodo.module.css';
+const AddTodo = () => {
+    return (
+        <a href="#" className={styles.a}>
+            Add Todo
+        </a>
+    );
+};
+
+export default AddTodo;
