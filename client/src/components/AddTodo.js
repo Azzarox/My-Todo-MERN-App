f1c7467d937bad49b5282d75d5ef36da67bbe7fb
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 15:51:09 +0300

    Fixed css styles applying to all of a tags

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,7 +1,7 @@
 import styles from './AddTodo.module.css';
 const AddTodo = () => {
     return (
-        <a href="#" className={styles.a}>
+        <a href="#" className={styles['btn-add-todo']}>
             Add Todo
         </a>
     );
