c51f7ba765fd4d3eb4a23413a5a51336bda54e25
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 11:18:48 +0300

    Updated the todosCatalog to load filter todos

--- client/src/components/TodosCatalog.js
+++ client/src/components/TodosCatalog.js
@@ -3,22 +3,35 @@ import TodoCard from './TodoCard';
 import * as todoServices from '../services/todoServices';
 import CustomSpinner from './Spinner';
 import Stack from 'react-bootstrap/Stack';
+import FilterTodos from './FilterTodos';
 
 const TodosCatalog = () => {
     const [todos, setTodos] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState('Recent');
 
     useEffect(() => {
         setTimeout(() => {
-            todoServices.getAllTodos().then((data) => {
+            // In the api filter should be lowercase
+            // But from the client it is received as uppercase (Recent, Incomplete, Completed);
+            // So have to make it lowerCase
+            todoServices.getAllTodos(filter.toLowerCase()).then((data) => {
                 setTodos(data);
                 setLoading(false);
             });
         }, 1000);
-    }, []);
+    }, [filter]);
+
+    // Changes filter func
+    function filterHandler(e) {
+        if (e.target.tagName === 'A') {
+            setFilter(e.target.text);
+        }
+    }
 
     return (
         <Stack direction="vertical" gap={3}>
+            <FilterTodos filterHandler={filterHandler} />
             {loading ? (
                 <CustomSpinner />
             ) : (
