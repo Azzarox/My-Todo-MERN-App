b13f7c87b7b75e9cc917b7c8259c6e981058e519
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 23:29:40 +0300

    Added modal form and create todo

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,9 +1,65 @@
 import styles from './AddTodo.module.css';
-const AddTodo = () => {
+
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
+const AddTodo = ({ updateTodos}) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:3001/api/todos', { method: 'POST' })
+            .then((res) => res.json())
+            .then((newTodo) => updateTodos(newTodo));
+    };
+
     return (
-        <a href="#" className={styles['btn-add-todo']}>
-            Add Todo
-        </a>
+        <>
+            <a href="#" className={styles['btn-add-todo']} onClick={handleShow}>
+                Add Todo
+            </a>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Todo</Modal.Title>
+                </Modal.Header>
+                <Form onSubmit={handleSubmit}>
+                    <Modal.Body>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlInput1"
+                        >
+                            <Form.Label>Title</Form.Label>
+                            <Form.Control type="text" autoFocus />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="light" onClick={handleClose}>
+                            Discard
+                        </Button>
+                        <Button
+                            type="submit"
+                            variant="dark"
+                            onClick={handleClose}
+                        >
+                            Create
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
+        </>
     );
 };
 
