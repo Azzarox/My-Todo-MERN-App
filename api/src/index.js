197049fe2aaba6cbfafd7d9294a7ac4e0f307d5e
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 15:31:15 +0300

    Imported config with the specified port for env

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
 const cors = require('cors');
 const router = require('./routes');
+
+const config = require('./config.json')[process.env.NODE_ENV];
 
 app.use(express.json());
 app.use(cors()); // Needed for api requests
 
 app.use('/api', router);
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+app.listen(config.PORT, () => {
+    console.log(`Server is listening on ${config.PORT}`);
 });
