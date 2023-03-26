18072b1ebf8a3497987c6c38a58c5ce6de04f98b
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 11:44:02 +0300

    Added title changing upon fitler by click

--- client/src/components/FilterTodos.js
+++ client/src/components/FilterTodos.js
@@ -1,13 +1,13 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
-function FilterTodos({ filterHandler }) {
+function FilterTodos({ filterHandler, filter}) {
     return (
         <DropdownButton
             onClick={filterHandler}
             variant="dark"
             id="dropdown-basic-button"
-            title="Filter by"
+            title={filter ? filter : "Filter by"}
         >
             <Dropdown.Item>Recent</Dropdown.Item>
             <Dropdown.Item>Incomplete</Dropdown.Item>
