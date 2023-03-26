2ac0611b74405b40ae7efd1d07ac08e8f1cd1142
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-26 11:31:44 +0300

    Added one more todo and cleaned comments

--- api/data/data.js
+++ api/data/data.js
@@ -4,7 +4,6 @@ const initialTodos = [
         title: 'Wash the dishes',
         description:
             'Wash the dishes in the sink. Do it earlier since there will be more waste',
-        // timestamp: '3/25/2023 3:11:27 PM GMT2',
         timestamp: '01/04/2022 8:23:00',
         isDone: false,
     },
@@ -13,7 +12,6 @@ const initialTodos = [
         title: 'Wash the dishes',
         description:
             'Wash the dishes in the sink. Do it earlier since there will be more waste',
-        // timestamp: '3/25/2023 3:12:49 PM GMT2',
         timestamp: '01/04/2022 11:45:59',
         isDone: true,
     },
@@ -22,8 +20,15 @@ const initialTodos = [
         title: 'Wash the dishes',
         description:
             'Wash the dishes in the sink. Do it earlier since there will be more waste',
-        // timestamp: '3/25/2023 3:13:16 PM GMT2',
         timestamp: '01/04/2022 15:30:00',
+        isDone: false,
+    },
+    {
+        id: 4,
+        title: 'Wash the dishes',
+        description:
+            'Wash the dishes in the sink. Do it earlier since there will be more waste',
+        timestamp: '04/04/2022 07:06:00',
         isDone: false,
     },
 ];
