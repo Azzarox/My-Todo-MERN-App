f6eb49792a597c288c302a7c7dd8e26035e88f85
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-28 10:59:19 +0300

    Fixed compare function to work properly

--- api/src/utils/compareTimestamps.js
+++ api/src/utils/compareTimestamps.js
@@ -1,15 +1,17 @@
+const moment = require('moment');
+
 function compareTimestamps(a, b) {
-    const date1 = new Date(a.timestamp);
-    const date2 = new Date(b.timestamp);
+    const date1 = moment(a.timestamp, 'DD/MM/YYYY HH:mm:ss');
+    const date2 = moment(b.timestamp, 'DD/MM/YYYY HH:mm:ss');
 
     if (a.isDone && !b.isDone) {
         return 1;
     } else if (!a.isDone && b.isDone) {
         return -1;
     } else {
-        if (date1.getTime() > date2.getTime()) {
+        if (date1.isAfter(date2)) {
             return -1;
-        } else if (date2.getTime() > date1.getTime()) {
+        } else if (date2.isAfter(date1)) {
             return 1;
         } else {
             return 0;
