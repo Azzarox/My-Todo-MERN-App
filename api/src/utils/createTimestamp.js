a8132f1d21b87eb992815e162b1dd66aa5d59e7e
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-28 10:59:06 +0300

    Fixed func to work with moment

--- api/src/utils/createTimestamp.js
+++ api/src/utils/createTimestamp.js
@@ -1,24 +1,7 @@
-// function createTimestamp() {
-//     const currentDate = new Date();
-//     const timestamp = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()} GMT${
-//         -currentDate.getTimezoneOffset() / 60
-//     }`;
-
-//     return timestamp;
-// }
+const moment = require('moment');
 
 function createTimestamp() {
-    const currentDate = new Date();
-    const options = {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZone: 'UTC',
-    };
-    const timestamp = currentDate.toLocaleDateString('en-GB', options);
+    const timestamp = moment().format('DD/MM/YYYY, HH:mm:ss');
     return timestamp;
 }
 
