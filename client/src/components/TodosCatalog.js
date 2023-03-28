3e00f3459cf6288a10884174147365430db041cf
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-28 10:53:03 +0300

    Added loading circle showing when filtering

--- client/src/components/TodosCatalog.js
+++ client/src/components/TodosCatalog.js
@@ -12,12 +12,13 @@ import AddTodo from './AddTodo';
 
 const TodosCatalog = () => {
     const [todos, setTodos] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState('');
     const [filter, setFilter] = useState('');
     const [title, setTitle] = useState('');
 
     // Call when filter is changed
     useEffect(() => {
+        setLoading(true);
         const timeoutId = setTimeout(() => {
             // In the api filter should be lowercase
             // But from the client it is received as uppercase (Recent, Incomplete, Completed);
