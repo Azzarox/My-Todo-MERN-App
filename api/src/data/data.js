157a33a407c8f2e904a2eacc3b8e25902b12c98e
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-28 10:58:25 +0300

    Fixed date format (added coma between date & hour)

--- api/src/data/data.js
+++ api/src/data/data.js
@@ -4,21 +4,21 @@ const initialTodos = [
         title: 'Wash the dishes',
         description:
             'Wash the dishes in the sink. Do it earlier since there will be more waste',
-        timestamp: '01/04/2022 8:23:00',
+        timestamp: '01/04/2022, 8:23:00',
         isDone: false,
     },
     {
         id: 2,
         title: 'Take a shower',
         description: '',
-        timestamp: '01/04/2022 11:45:59',
+        timestamp: '01/04/2022, 11:45:59',
         isDone: true,
     },
     {
         id: 3,
         title: 'Clean the living room',
         description: 'Clean under the sofa and table',
-        timestamp: '01/04/2022 15:30:00',
+        timestamp: '01/04/2022, 15:30:00',
         isDone: false,
     },
     {
@@ -26,7 +26,7 @@ const initialTodos = [
         title: 'Go to the market',
         description:
             'Go to the market and buy food. There is a list of ingredients at the fridge door.',
-        timestamp: '04/04/2022 07:06:00',
+        timestamp: '04/04/2022, 07:06:00',
         isDone: false,
     },
 ];
