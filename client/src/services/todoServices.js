b993586c0e711a546f75a7e82d4912ace9c6b402
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-28 11:57:13 +0300

    Added service for deleting todo

--- client/src/services/todoServices.js
+++ client/src/services/todoServices.js
@@ -18,8 +18,14 @@ export const getAllTodosByTitle = (title) => {
         res.json()
     );
 };
-export const completeTodo = (todo) => {
-    return fetch(`http://localhost:3001/api/todos/${todo.id}`, {
+export const completeTodo = (id) => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
         method: 'PUT',
+    }).then((res) => res.json());
+};
+
+export const deleteTodo = (id) => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
+        method: 'DELETE',
     }).then((res) => res.json());
 };
