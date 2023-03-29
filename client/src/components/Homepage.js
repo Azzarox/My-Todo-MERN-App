5bb3eb8747fbb74918608d7b9eda71291aaf07c7
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-29 17:20:33 +0300

    Added Container (bootstrap) for better scaling

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,15 +1,16 @@
 import TodosCatalog from './TodosCatalog';
 
 import styles from './Homepage.module.css';
+import { Container } from 'react-bootstrap';
 
 const Homepage = () => {
     return (
-        <>
+        <Container fluid>
             <div className={styles['site-wrapper']}>
                 <h1>My Todos</h1>
                 <TodosCatalog />
             </div>
-        </>
+        </Container>
     );
 };
 
