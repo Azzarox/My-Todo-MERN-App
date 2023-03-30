e29e9bdf29445082f079a7e36fbba0f654e29641
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-30 18:27:29 +0300

    Added services for querying the db

--- api/src/apiServices/todoServices.js
+++ api/src/apiServices/todoServices.js
@@ -1,4 +1,5 @@
 const initialTodos = require('../data/data');
+const Todo = require('../models/Todo');
 
 const getAllTodos = () => {
     return initialTodos;
@@ -12,5 +13,22 @@ const todoServices = {
     getAllTodos,
     getTodo,
 };
+
+const db_getAll = () => {
+    return Todo.findOne(); // Returns promise
+};
+
+const db_getOne = (id) => {
+    return Todo.findById(id)
+}
+
+
+// db_getAll()
+//     .then((docs) => console.log(docs))
+//     .catch((err) => console.log(err));
+
+// db_getOne("642577d34c18cc939d2b5783")
+//     .then((docs) => console.log(docs))
+//     .catch((err) => console.log(err));
 
 module.exports = todoServices;
