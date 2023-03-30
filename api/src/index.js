9474ccd41b77b399d3e2faf7a47859ccc3df62a7
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-03-30 14:33:35 +0300

    Initializing database before connection to server

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const config = require('./config.json')[process.env.NODE_ENV];
+const initializeDatabase = require('./config/mongoose');
+
 const app = express();
+
 const cors = require('cors');
 const router = require('./routes');
 
-const config = require('./config.json')[process.env.NODE_ENV];
 
 app.use(express.json());
 app.use(cors()); // Needed for api requests
 
 app.use('/api', router);
 
-app.listen(config.PORT, () => {
-    console.log(`Server is listening on ${config.PORT}`);
-});
+initializeDatabase(config.DATABASE_URL)
+    .then(() => {
+        app.listen(config.PORT, () => {
+            console.log(`Server is listening on ${config.PORT}`);
+        });
+    })
+    .catch((err) => console.log(`There was error with database ${err}`));
