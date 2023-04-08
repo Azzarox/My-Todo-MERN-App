64e235ccbfb9e2adbbac791b61c140a47cad6759
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-08 13:52:30 +0300

    Added auth.login service

--- api/src/apiServices/authServices.js
+++ api/src/apiServices/authServices.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('../utils/jwt');
 
 const register = async (username, password) => {
     const existingUser = await User.findOne({ username });
@@ -6,12 +7,31 @@ const register = async (username, password) => {
     if (existingUser) {
         throw new Error('User already exists!');
     }
-    
+
     return User.create({ username, password });
+};
+
+const login = async (username, password) => {
+    const user = await User.findOne({ username });
+    if (!user) {
+        throw new Error("User with that username doesn't exist");
+    }
+
+    // Need to await to pass the error if such
+    const isValid = await user.validatePassword(password);
+    if (!isValid) {
+        throw new Error("Username or password doesn't match");
+    }
+
+    const payload = { id: user._id, username: user.username };
+    const token = await jwt.sign(payload, 'ASDSADSAD');
+
+    return token;
 };
 
 const authServices = {
     register,
+    login,
 };
 
 module.exports = authServices;
