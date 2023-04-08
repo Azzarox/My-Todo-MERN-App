e5bc6f35276691f06197be9ed06f1959f15990cc
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-08 16:55:21 +0300

    Added UI for Register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,79 @@
+import { useState } from 'react';
+import * as authServices from '../services/authServices';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Alert, Col, Row } from 'react-bootstrap';
+
 const Register = () => {
-    return <h1>Register</h1>;
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    async function onSubmitRegister(ev) {
+        ev.preventDefault();
+        const formData = new FormData(ev.currentTarget);
+        const { username, password, repass } = Object.fromEntries(formData);
+
+        const data = await authServices.register({
+            username,
+            password,
+            repass,
+        });
+
+        if (data.id) {
+            setUser(data);
+        } else {
+            setError(data);
+        }
+
+        console.log(user);
+        console.log(error);
+    }
+
+    return (
+        <Form onSubmit={onSubmitRegister} className="wrapper">
+            {error ? (
+                <>
+                    <Alert className="flex fl-between" variant="danger">
+                        {error.err}
+                        <i
+                            onClick={() => setError(null)}
+                            className="bi bi-x-lg"
+                        ></i>
+                    </Alert>
+                </>
+            ) : null}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Repeat Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="repass"
+                />
+            </Form.Group>
+
+            <Button variant="dark" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
 };
 
 export default Register;
