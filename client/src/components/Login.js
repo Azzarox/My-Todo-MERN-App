292c5ebf60089c7c75269dd9dba4d8cd8f74d961
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-08 17:36:28 +0300

    Added Login functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,50 @@
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+import * as authServices from '../services/authServices';
+import { Alert } from 'react-bootstrap';
+
 function Login() {
+    const [token, setToken] = useState(null);
+    const [err, setErr] = useState(null);
+
+    async function onSubmitLogin(ev) {
+        ev.preventDefault();
+        const formData = new FormData(ev.currentTarget);
+        const { username, password } = Object.fromEntries(formData);
+        const data = await authServices.login({
+            username,
+            password,
+        });
+
+        if (data.token) {
+            setToken(data);
+        } else {
+            setErr(data);
+        }
+    }
+
+    // Setting to localStorage
+    localStorage.setItem('token', JSON.stringify(token?.token));
+
     return (
-        <Form className="wrapper">
+        <Form onSubmit={onSubmitLogin} className="wrapper">
+            {err ? (
+                <>
+                    <Alert className="flex fl-between" variant="danger">
+                        {err.err}
+                        <i
+                            onClick={() => setErr(null)}
+                            className="bi bi-x-lg"
+                        ></i>
+                    </Alert>
+                </>
+            ) : null}
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Username</Form.Label>
                 <Form.Control
-                    type="email"
+                    type="text"
                     placeholder="Username"
                     name="username"
                 />
