3081a1ac821e3cf394c96bce6aa9f012e3c9d433
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-08 16:55:45 +0300

    Added UI without functionality for login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,31 @@
-const Login = () => {
-    return <h1>Login</h1>;
-};
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function Login() {
+    return (
+        <Form className="wrapper">
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Username"
+                    name="username"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+            </Form.Group>
+            <Button variant="dark" type="submit">
+                Login
+            </Button>
+        </Form>
+    );
+}
 
 export default Login;
