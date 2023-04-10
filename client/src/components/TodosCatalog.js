72010be353bbd51812e24d016a128e2ba1753965
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-10 13:12:36 +0300

    useEffect call now catches error and redirects

--- client/src/components/TodosCatalog.js
+++ client/src/components/TodosCatalog.js
@@ -10,24 +10,28 @@ import SearchTodo from './SearchTodo';
 import debounce from '../utils/debouncer';
 import AddTodo from './AddTodo';
 import { Alert, Button } from 'react-bootstrap';
-
+import { useNavigate } from 'react-router-dom';
 const TodosCatalog = () => {
     const [todos, setTodos] = useState([]);
     const [loading, setLoading] = useState('');
     const [filter, setFilter] = useState('');
     const [title, setTitle] = useState('');
+    const navigate = useNavigate();
 
-    // Call when filter is changed
     useEffect(() => {
         setLoading(true);
         const timeoutId = setTimeout(() => {
-            // In the api filter should be lowercase
-            // But from the client it is received as uppercase (Recent, Incomplete, Completed);
-            // So have to make it lowerCase
-            todoServices.getAllTodos(filter.toLowerCase()).then((data) => {
-                setTodos(data);
-                setLoading(false);
-            });
+            todoServices
+                .getAllTodos(filter.toLowerCase())
+                .then((data) => {
+                    // This will be executed only if there is no error
+                    setTodos(data);
+                    setLoading(false);
+                })
+                .catch(
+                    // if there is error navigate to the login page
+                    (err) => navigate('/login')
+                );
         }, 1000);
 
         // To avoid memory leak?
