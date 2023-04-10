cda521cb13d0efe0a7b6cc249af40c195d775675
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-10 13:14:16 +0300

    Refactored to throw err if not okay response

--- client/src/services/todoServices.js
+++ client/src/services/todoServices.js
@@ -9,10 +9,24 @@ const routes = {
     },
 };
 
-export const getAllTodos = (filter) => {
-    // This works even if filter is empty string because of the api default /api/todos
-    return fetch(apiUrl + routes.allTodos(filter)).then((res) => res.json());
+export const getAllTodos = async (filter) => {
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    const response = await fetch(apiUrl + routes.allTodos(filter), {
+        headers: {
+            Authorization: 'Bearer ' + token,
+        },
+    });
+
+    // The response will be unauthorized so we throw the error back to be caught in the useEffect in TodoCatalog
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    // If the response is ok we get the data
+    return response.json();
 };
+
 export const getAllTodosByTitle = (title) => {
     return fetch(apiUrl + routes.allTodosByTitle(title)).then((res) =>
         res.json()
