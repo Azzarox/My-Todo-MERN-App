da64dff7ec34c632af275a905c757c8ddbd710bd
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-10 10:21:41 +0300

    Added todos field for the model

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    todos: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Todo',
+    },
 });
 
 // Made so " asdasd  " is "asdasd";
