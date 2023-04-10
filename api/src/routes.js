1eb6cd4e395aace1cac7f71ab41b989bbe902066
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-10 13:10:35 +0300

    Added the authMiddleware only for /todos path

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const todoController = require('./controllers/todoController');
 const authController = require('./controllers/authController');
 
-router.use('/', todoController);
+const authMiddleware = require('./middlewares/authMiddleware');
+
 router.use('/auth', authController);
+router.use('/todos', authMiddleware, todoController);
 
 module.exports = router;
