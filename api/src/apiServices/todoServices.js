15d2fb5b18d7cd4611f7739384e495a6295a1225
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-12 17:53:35 +0300

    Fixed the search service to look for results only in the selected filter

--- api/src/apiServices/todoServices.js
+++ api/src/apiServices/todoServices.js
@@ -23,11 +23,32 @@ const getTodosByRecentWithFilter = (authorId, filter) => {
     }
 };
 
-const getTodosByQuery = (authorId, query) => {
+const getTodosByQuery = (authorId, query, filter) => {
     const regex = new RegExp(query, 'i'); // create regex for case-insensitive search
-    return Todo.find({ author: authorId, title: { $regex: regex } }).sort({
-        createdAt: -1,
-    });
+    if (filter === 'incomplete') {
+        return Todo.find({
+            author: authorId,
+            title: { $regex: regex },
+            isDone: false,
+        }).sort({
+            createdAt: -1,
+        });
+    } else if (filter === 'completed') {
+        return Todo.find({
+            author: authorId,
+            title: { $regex: regex },
+            isDone: true,
+        }).sort({
+            createdAt: -1,
+        });
+    } else if (filter === 'recent' || !filter) {
+        return Todo.find({
+            author: authorId,
+            title: { $regex: regex },
+        }).sort({
+            createdAt: -1,
+        });
+    }
 };
 
 const todoServices = {
