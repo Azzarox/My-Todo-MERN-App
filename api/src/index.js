0f33548f44e179932c9c9e54d81978787f3952ee
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-13 10:19:46 +0300

    Required the dotenv package

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const config = require('./config.json')[process.env.NODE_ENV];
+
+require('dotenv').config();
+const config = require('./config')[process.env.NODE_ENV];
+
 const initializeDatabase = require('./config/mongoose');
 
 const app = express();
 
 const cors = require('cors');
 const router = require('./routes');
-
 
 app.use(express.json());
 app.use(cors()); // Needed for api requests
