6e837bda558279c7a2554f5d02bf563678cdf2d8
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-15 16:43:46 +0300

    Using the swagger

--- api/src/index.js
+++ api/src/index.js
@@ -2,12 +2,13 @@ const cors = require('cors');
 const express = require('express');
 
 require('dotenv').config();
-
 const config = require('./config')[process.env.NODE_ENV];
 const initializeDatabase = require('./config/mongoose');
 
 const app = express();
 const router = require('./routes');
+
+require('./config/swagger.config')(app);
 
 app.use(express.json());
 app.use(cors()); // Needed for api requests
