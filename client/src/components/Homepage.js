75a5b39cad284d9199df4d0792002d5fe829d913
Azzarox <62171309+Azzarox@users.noreply.github.com>
2023-04-25 12:16:35 +0300

    Added hook for updating the username on the Navbar

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,7 +3,17 @@ import TodosCatalog from './TodosCatalog';
 import styles from './Homepage.module.scss';
 import { Container } from 'react-bootstrap';
 
-const Homepage = () => {
+import * as authServices from '../services/authServices';
+import { useEffect } from 'react';
+
+const Homepage = ({ updateUser }) => {
+    useEffect(() => {
+        authServices
+            .getUserInfo()
+            .then((userData) => updateUser(userData))
+            .catch((err) => console.error(err.message));
+    }, []);
+
     return (
         <Container fluid>
             <div className={styles['site-wrapper']}>
